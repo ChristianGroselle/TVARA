3d169fce10b051be5fc0e1d45fc79e53a6552650
ChristianGroselle <christian.groselle@gmail.com>
2022-10-06 20:33:52 -0400

    added basic implementaion of Edamam api. no current functional use

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//edamam api testing
+//test request url
+//https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eaff234d&app_key=4726246f26709a39dee3c8328f230c5e%09&mealType=Dinner&field=url
+
+var apiTestEl = document.querySelector('#apiCall');
+
+function getApi() {
+    // replace `octocat` with anyone else's GitHub username
+    var requestUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eaff234d&app_key=4726246f26709a39dee3c8328f230c5e%09&mealType=Dinner&field=url';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        console.log(data.hits.length);
+        for (var i = 0; i < data.hits.length; i++) {
+        console.log(data[i]);
+        var listItem = document.createElement('li');
+        listItem.textContent = data.hits[i].recipe.url;
+        apiTestEl.appendChild(listItem);
+        }
+      });
+  }
+
+  getApi();
