d7e0a7192bffab29af55dd98e13e6245e2df5047
ChristianGroselle <christian.groselle@gmail.com>
2022-10-07 04:19:24 -0400

    added functinality for building result cards in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,12 @@
 //https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eaff234d&app_key=4726246f26709a39dee3c8328f230c5e%09&mealType=Dinner&field=url
 
 var apiTestEl = document.querySelector('#apiCall');
+var recipeCardListEl = $('#recipeList');
+var searchBtnEl = $('#searchBtn');
+var temp = 0;
 
 //initialization for materialize js
 M.AutoInit();
-
-//$(document).ready(function(){
-//  $('select').formSelect();
-//});
 
 // gets the url of 20 dinner recipies that have to do with chicken
 //doesnt have a functional use yet
@@ -33,4 +32,20 @@ function getApi() {
       });
   }
 
+  function buildRecipeCard(rTitle, rDesc, rLink, rImg) {
+    temp++;
+    console.log(temp);
+    recipeCardListEl.append('<li><div class="card-panel grey lighten-5 z-depth-1"><div class="row valign-wrapper"><div class="col s4"><img src="'+rImg+'" alt="" class="circle responsive-img"></div><div class="col s8"><span class="black-text"><h4>'+rTitle+'</h4><div>'+rDesc+'</div></span></div></div></div></li>');
+  }
+// for(let i = 0; i < 10;i++){
+//   let temp = i + 1
+//   $('#startBtn').click(buildRecipeCard('Recipe Title '+ temp, 'Recipe description','','../images/200.jfif'));
+// }
+
+// $('#searchBtn').click(buildRecipeCard('Recipe Title '+ temp, 'Recipe description','','../images/200.jfif'));
+
+//searchBtnEl.click(buildRecipeCard('Recipe Title '+ temp, 'Recipe description','','../images/200.jfif'));
+for (let index = 0; index < 10; index++) {
+  buildRecipeCard('Recipe Title '+ temp, 'Recipe description','','../images/200.jfif')
+}
  //getApi();
